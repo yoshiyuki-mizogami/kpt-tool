7238d8108ae665f53f8f420d60a12bd7608f1048
Yoshiyuki Mizogami <yoshiyuki.mizogami@ani-com.com>
2019-07-04 12:27:23 +0900

    save

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const ex = require('express')
 const PORT = process.env.PORT || 5000
 const {join} = require('path')
+const {readFileSync} = require('fs')
 ex()
-  .use(ex.static(path.join(__dirname, 'statics')))
+  .use(ex.static(join(__dirname, 'statics')))
+  .get('/',(req, res)=>{
+    res.end(readFileSync(join(__dirname, 'statics', 'app', 'index.html'), 'utf8'))
+  })
   .listen(PORT, ()=>{
-    
+    console.log('start kpt tool server')
   })
